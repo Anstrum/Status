c6b2f9cbfb9df61be7faea98a6882640885a7b4e
Jaetan-Salvetat <gaetansalvi08@gmail.com>
2022-04-01 11:11:54 +0200

    fix navigation bug

--- app/src/main/java/com/aos/status/services/Navigation.kt
+++ app/src/main/java/com/aos/status/services/Navigation.kt
@@ -2,34 +2,39 @@ package com.aos.status.services
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import com.aos.status.R
 
 class Navigation {
     companion object {
-        private var history: MutableList<Fragment> = mutableListOf()
-        private var mainLayout: Int = R.id.main_layout
+        private var history: ArrayList<Fragment> = arrayListOf()
+        private var mainLayout: Int = 0
         private lateinit var supportFragmentManager: FragmentManager
 
-        fun init(parent: FragmentManager, mainFragment: Fragment, layout: Int? = null){
+        fun init(parent: FragmentManager, mainFragment: Fragment, layout: Int){
             supportFragmentManager = parent
             push(mainFragment)
-            if(layout != null) mainLayout = layout
+            mainLayout = layout
         }
 
         fun push(fragment: Fragment){
             history.add(fragment)
+            navigate()
+        }
+
+        fun back(): Boolean {
+            if(history.isEmpty() || history.count() == 1) return false
+
+            history.removeLastOrNull()
+            navigate()
+
+            return true
+        }
+
+
+        private fun navigate(){
             supportFragmentManager
                 .beginTransaction()
                 .replace(mainLayout, history.last())
                 .commit()
-        }
-
-        fun back(): Boolean {
-            if(history.isEmpty() || history.count() == 1)  return false
-            history.removeLastOrNull()
-            push(history.last())
-
-            return true
         }
     }
 }
